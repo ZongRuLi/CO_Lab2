f339fdcf4eca77708a28dcdde59f900441ed20da
ZongRuLi <monja4564@outlook.com>
2019-05-07 15:02:16 +0800

    fix myTestbranch k++ => k+1

--- code/myTestBench.v
+++ code/myTestBench.v
@@ -81,7 +81,7 @@ initial  begin
 
 	// 讀取 測試指令 到 cpu指令記憶體 中
 	$readmemb("CO_P2_test_data1.txt", cpu.IM.Instr_Mem);  //Read instruction from "CO_P2_test_data1.txt" 
-    for(k=0;k<7;k++)
+    for(k=0;k<7;k=k+1)
 		$display("%b",cpu.IM.Instr_Mem[k]);	
 	// 讀取 驗證輸出
 	handle = $fopen("CO_P2_result.txt");
@@ -95,7 +95,7 @@ initial  begin
 	RST = 1;
 	pc = 32'd0;
 	
-	for(k=0;count != `END_COUNT;k++)begin
+	for(k=0;count != `END_COUNT;k=k+1)begin
 		instruction = cpu.IM.Instr_Mem[ pc>>2 ];
 		pc = pc + 32'd4;
 		
